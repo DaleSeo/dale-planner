1fd0300bc45c61e310ee262c237ebc611d17f2fd
Dale Seo <dale.seo@gmail.com>
2017-06-29 20:28:38 +0900

    have bookmarks as a menu item

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,36 +1,54 @@
 <template>
-  <div class="layout">
-    <div class="ui left vertical menu sidebar">
-      <a class="item">
-        Item 1
-      </a>
-      <a class="item">
-        Item 2
-      </a>
-      <a class="item">
-        Item 3
-      </a>
+  <div id="app" class="ui segment pushable">
+    <div class="ui inverted vertical icon labeled menu sidebar">
+      <div class="item" @click="select('home')">
+        <i class="home icon"/>Dale's Home
+      </div>
+      <div class="item" @click="select('planner')">
+        <i class="checked calendar icon"/>Planner
+      </div>
+      <div class="item" @click="select('bookmarks')">
+        <i class="bookmark icon"/>Bookmarks
+      </div>
     </div>
     <div class="pusher">
-      <div class="ui button" @click="toggleMenu">Menu</div>
-      <Home id="app"/>
+      <div class="ui basic segment">
+        <div class="ui icon button" @click="toggleMenu">
+          <i class="sidebar icon"/>
+        </div>
+        <Bookmarks v-show="menuItem === 'bookmarks'"/>
+        <Home v-show="menuItem !== 'bookmarks'"/>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import Home from './components/Home.vue'
+import Bookmarks from './Bookmarks.vue'
 
 export default {
-  components: {Home},
+  components: {Home, Bookmarks},
+  data () {
+    return {
+      menuItem: 'home'
+    }
+  },
+  mounted () {
+    $('#app .ui.sidebar')
+      .sidebar({
+        context: $('#app')
+      })
+      .sidebar('attach events', '#app .menu .item')
+  },
   methods: {
     toggleMenu () {
-      $('.ui.sidebar')
-        .sidebar({
-          context: $('.layout')
-        })
-        .sidebar('attach events', '.layout .menu .item')
+      $('#app .ui.sidebar')
         .sidebar('toggle')
+    },
+    select (menuItem) {
+      console.log('#selected:', menuItem)
+      this.menuItem = menuItem
     }
   }
 }
